7a946dad8f934939a71958cefab31bb0c1211b3c
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-31 07:47:36 -0400

    WOOOOP

--- makeText.js
+++ makeText.js
@@ -1,2 +1,44 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+//generate text from markov machine
+const generateText = (text) => {
+    let machina = new markov.MarkovMachine(text);
+    console.log(machina.makeText());
+}
+
+//read file and generate text
+const makeText = (path) => {
+    fs.readFile(path, 'utf8', function(err, data){
+        if(err){
+            console.log(`Cannot read file: ${path}: ${err}`);
+            process.exit(1);
+        } generateText(data);
+    })
+}
+
+//read url and make text
+async function makeTextURL(url){
+    let res;
+    try{
+        res = await axios.get(url);
+    } catch(err){
+        console.log(`Cannot read file: ${path}: ${err}`);
+        process.exit(1);
+    } makeText(res.data);
+}
+
+//interpret command line and go from there
+let [method,path] = process.argv.slice(2);
+if(method === "file"){
+    makeText(path);
+} else if (method === "url"){
+    makeTextURL(path);
+} else {
+    console.log(`METHOD UNKNOWN: ${method}`);
+    process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -18,12 +18,37 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let chains = new Map();
+
+    for(let i = 0; i < this.words.length; i++){
+      let word = this.words[i];
+      let next = this.words[i + 1] || null;
+
+      if(chains.has(word)){
+        chains.get(word).push(nextWord);
+      } chains.set(word, [next]);
+    }
+    this.chains = chains;
   }
 
+  static choice(arr){
+    //pick random choice from arr
+    return ar[Math.floor(Math.random() * arr.length)]
+  }
 
   /** return random text from chains */
-
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    //markov chain until termination word
+    while(out.length < numWords && key != null){
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    } return out.join(" ");
   }
 }
+
+module.exports = {MarkovMachine}
